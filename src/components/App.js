4aaca5ddd1f9cfa87bbccb2f0aa0880a1a48aae2
Justin Chau <chausicle@gmail.com>
2020-06-18 22:00:29 -0700

    Add api folder for axios configurations

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      headers: {
-        Authorization:
-          "Client-ID fd0d04e720b832fdbefba3c5f1fd70ddfadd3434a171f9c8398ccea0bc6f0232",
-      },
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
       params: { query: term },
     });
 
-    console.log(response.data.results);
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }

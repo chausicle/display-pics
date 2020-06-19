b5f0122e7ddb8256d7eddb840a3f877ffe7c8dfa
Justin Chau <chausicle@gmail.com>
2020-06-18 20:16:31 -0700

    Add AJAX request to unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization:
+          "Client-ID fd0d04e720b832fdbefba3c5f1fd70ddfadd3434a171f9c8398ccea0bc6f0232",
+      },
+      params: { query: term },
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
